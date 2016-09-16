c0e2d5999da33fddff7c1f7d1ce58f81ed94ac0e
logictkt <logictkt@gmail.com>
2016-09-16 22:29:40 +0900

    add rumoji

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,14 @@
 class Comment < ApplicationRecord
+  before_validation :convert_rumoji
+  validates :body, presence: true
+
+  def decode_body
+    Rumoji.decode(self.body)
+  end
+
+  private
+
+  def convert_rumoji
+    self.body = Rumoji.encode(self.body)
+  end
 end
